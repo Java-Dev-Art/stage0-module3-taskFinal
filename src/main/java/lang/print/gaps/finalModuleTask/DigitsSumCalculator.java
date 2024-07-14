920232c030600d611e20a202ba92f9a0c090eadf
Artur Markouski <markouski.artur@student.ehu.lt>
2024-07-14 21:58:36 +0300

    feat: solving 5 task

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,17 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        char[] mas = Integer.toString(number).toCharArray();
+        int result = 0;
+        for (int i = 0; i < mas.length ; i++){
+            String beforeInt = String.valueOf(mas[i]);
+            int beforeResult = Integer.valueOf(beforeInt);
+            result += beforeResult;
+        }
+        System.out.println(result);
+    }
+    public static void main(String[] arg){
+        DigitsSumCalculator cal = new DigitsSumCalculator();
+        cal.calculateSum(1111);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        char[] mas = Integer.toString(number).toCharArray();
+        StringBuilder result = new StringBuilder();
+        for (int i = mas.length -1; i >= 0; i--){
+            result.append(mas[i]);
+        }
+        System.out.println(result);
     }
 }
