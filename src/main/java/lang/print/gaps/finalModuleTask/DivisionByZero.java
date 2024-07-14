5c4ede63f0593619dbe87d25a68653693ca69f6f
Artur Markouski <markouski.artur@student.ehu.lt>
2024-07-14 23:58:40 +0200

    feat: solving add 2 tasks

--- src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
@@ -3,7 +3,7 @@ package lang.print.gaps.finalModuleTask;
 public class DivisionByZero {
     public static void main(String[] args) {
         int number = 10;
-        double result = number / 0;
+        double result = number / 0.0;
         System.out.println(result);
     }
 }
